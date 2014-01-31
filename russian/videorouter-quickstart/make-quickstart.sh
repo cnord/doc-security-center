7af84736db2397e6cf952b10d726da53be53b0d3
anzhu <anzhu@cnord.ru>
2014-01-31 16:46:57 +0400

    Видеороутер переехал в отдельный репозиторий (https://github.com/cnord/vr-doc)

--- russian/videorouter-quickstart/make-quickstart.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-pandoc --template template/template-manual.latex -V lang=russian -o VR-QuickStart-Rus.pdf 01-engineer-check-list.md 02-engineer-faq-01-network.md 02-engineer-faq-02-camera.md 02-engineer-faq-03-security-center.md 02-engineer-faq-04-private-cloud.md
