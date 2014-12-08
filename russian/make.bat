1ecafb7ab734ff63bff737339644b2e9912aea45
anzhu <anzhu@cnord.ru>
2014-12-09 01:12:09 +0300

    Мелкие правки перед релизом пятерки

--- russian/make.bat
+++ russian/make.bat
@@ -1,3 +1,3 @@
-pandoc --template template/template-install.latex -V lang=russian -o SC4-InstallationManual-Rus.pdf 01-introduction.md 02-installation.md
+pandoc --template template/template-install.latex -V lang=russian -o SC5-InstallationManual-Rus.pdf 01-introduction.md 02-installation.md
 
-pandoc --template template/template-manual.latex -V lang=russian -o SC4-UserManual-Rus.pdf *.md
+pandoc --template template/template-manual.latex -V lang=russian -o SC5-UserManual-Rus.pdf *.md
