e8be5825769ccd2690c75830f9c76c95a9669c89
anzhu <anzhu@cnord.ru>
2018-04-29 12:41:26 +0300

    PDF-RU собирается!
    Убрал все конфликты, собрал PDF

--- russian/make-doc.sh
+++ russian/make-doc.sh
@@ -1,6 +1,18 @@
 #!/bin/sh
 
+START=$(date +%s)
+ 
 pandoc --template template/template-install.latex -V lang=russian -o SC5-InstallationManual-Rus.pdf 01-introduction.md 02-installation.md
+ 
+END=$(date +%s)
+DIFF=$(( $END - $START ))
+echo "Instalation manual tooks $DIFF seconds"
+
+START=$(date +%s)
 
 pandoc --template template/template-manual.latex -V lang=russian -o SC5-UserManual-Rus.pdf 00-whats-new.md 01-introduction.md 02-installation.md 03-getting-started.md 04-event-manager.md 05-object-manager.md 06-system-setup.md 07-personnel-manager.md 08-maps-of-objects.md 09-duty-operator.md 10-report-manager.md 11-database-wizard.md 12-cloud-services.md 13-outro.md
+
+END=$(date +%s)
+DIFF=$(( $END - $START ))
+echo "User manual tooks $DIFF seconds"
 
