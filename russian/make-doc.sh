760739abe4ce38b533ec620fa2e8ff2f6b08e2d3
Максим Чистяков <mchistyakov@cnord.ru>
2019-04-22 15:02:32 +0300

    - добавлено автоматическое формирование руководства по установке SQL

--- russian/make-doc.sh
+++ russian/make-doc.sh
@@ -10,6 +10,15 @@ echo "Russian instalation manual – $DIFF seconds"
 
 START=$(date +%s)
 
+pandoc --template template/template.latex -V title-source=title-install-sql.tex -o SC5-Installation-SQL-Rus.pdf "setup SQL\02-1-installation_sql.md"
+
+END=$(date +%s)
+DIFF=$(( $END - $START ))
+echo "Russian instalation SQL manual – $DIFF seconds"
+
+
+START=$(date +%s)
+
 pandoc --template template/template.latex -V title-source=title-manual.tex -o sc5-usermanual-rus.pdf 00-whats-new.md 01-introduction.md 02-installation.md 03-getting-started.md 04-event-manager.md 05-object-manager.md 06-system-setup.md 07-personnel-manager.md 08-maps-of-objects.md 09-duty-operator.md 10-report-manager.md 11-database-wizard.md 12-cloud-services.md 13-outro.md
 
 END=$(date +%s)
